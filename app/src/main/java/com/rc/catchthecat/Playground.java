e1e40b1330ac3c1eca7ac6f159a46c1639476cd0
RC_Kanashii <cc1515781862@163.com>
2022-01-06 09:20:41 +0800

    Feat: Draw the map in a staggered way

--- app/src/main/java/com/rc/catchthecat/Playground.java
+++ app/src/main/java/com/rc/catchthecat/Playground.java
@@ -3,6 +3,8 @@ package com.rc.catchthecat;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -16,7 +18,11 @@ public class Playground extends SurfaceView {
     private final int ROW = 10;
     // 列数
     private final int COL = 10;
-    // // 默认添加的路障数量
+    // Dot直径
+    private int DOT_D = 80;
+    // 偏移量
+    private int OFFSET;
+    // 默认添加的路障数量
     private final int NUM_BARRIERS = 10;
     // 地图
     private Dot[][] map;
@@ -58,6 +64,7 @@ public class Playground extends SurfaceView {
         }
         // cat状态为CAT，并设置起始点
         cat = new Dot(4, 5, Status.CAT);
+        getDot(4, 5).setStatus(Status.CAT);
         // 初始化路障
         for (int i = 0; i < NUM_BARRIERS; ) {
             int x = random.nextInt(10);
@@ -66,7 +73,7 @@ public class Playground extends SurfaceView {
             if (dot.getStatus() == Status.EMPTY) {
                 dot.setStatus(Status.BARRIER);
                 i++;
-                Log.i("Random Barriers", String.format("x = %d, y = %d, i = %d", x, y ,i));
+                Log.i("Random Barriers", String.format("x = %d, y = %d, i = %d", x, y, i));
             }
         }
     }
@@ -75,6 +82,39 @@ public class Playground extends SurfaceView {
         // 获取Canvas对象
         Canvas canvas = getHolder().lockCanvas();
         canvas.drawColor(Color.CYAN);
+
+        // 绘制Dot
+        Paint paint = new Paint();
+        paint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        for (int i = 0; i < ROW; i++) {
+            // 设置奇偶行的偏移量
+            OFFSET = i % 2 == 0 ? 0 : DOT_D / 2;
+            for (int j = 0; j < COL; j++) {
+                Dot dot = getDot(j, i);
+                // 设置画笔颜色
+                switch (dot.getStatus()) {
+                    case EMPTY:
+                        paint.setColor(getResources().getColor(R.color.colorEmpty));
+                        break;
+                    case BARRIER:
+                        paint.setColor(getResources().getColor(R.color.colorBarrier));
+                        break;
+                    case CAT:
+                        paint.setColor(getResources().getColor(R.color.colorCat));
+                        break;
+                    default:
+                        paint.setColor(Color.LTGRAY);
+                        break;
+                }
+                canvas.drawOval(
+                        new RectF(dot.getX() * DOT_D + OFFSET,
+                                dot.getY() * DOT_D,
+                                (dot.getX() + 1) * DOT_D + OFFSET,
+                                (dot.getY() + 1) * DOT_D),
+                        paint);
+            }
+        }
+
         // 将画布内容进行提交
         getHolder().unlockCanvasAndPost(canvas);
     }
@@ -87,8 +127,11 @@ public class Playground extends SurfaceView {
         }
 
         @Override
-        public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
+        public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int format, int width, int height) {
+            // 动态设置Dot直径
+            DOT_D = width / (COL + 1);
+            // 重新绘制界面
+            redraw();
         }
 
         @Override
